bd15f2863dd7e8cfc0d6f873dfd5743d391835a1
Junior Tan <junior.tan@live.com>
2025-02-15 23:24:02 +0800

    auth config

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,17 +2,8 @@ import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import {
-  accounts,
-  sessions,
-  users,
-  verificationTokens,
-} from "~/server/db/schema";
 import { env } from "~/env";
 
-import { v4 as uuid } from "uuid";
-import { encode as defaultEncode } from "next-auth/jwt";
 
 import { db } from "~/server/db";
 
@@ -32,10 +23,10 @@ declare module "next-auth" {
     } & DefaultSession["user"];
   }
 
-  interface User {
-    // ...other properties
-    // role: string;
-  }
+  // interface User {
+  //   // ...other properties
+  //   // role: string;
+  // }
 }
 
 /**
@@ -83,7 +74,7 @@ export const authConfig = {
           role: user.role,
         };
       },
-      type: "credentials",
+      // type: "credentials",
     }),
     /**
      * ...add more providers here.
@@ -95,61 +86,6 @@ export const authConfig = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-        // role: user.role
-      },
-    }),
-    jwt: ({ token, account }) => {
-      console.log(`[config][jwt][token]: ${JSON.stringify(token, null, 2)}`)
-      console.log(`[config][jwt][account]: ${JSON.stringify(account, null, 2)}`)
-      if (account?.provider == "credentials") {
-        token.credentials = true;
-      }
-      return token;
-    },
-  },
-  jwt: {
-    encode: async (params) => {
-      console.log(`[config][encode][params]: ${JSON.stringify(params, null, 2)}`)
-      if (params.token?.credentials) {
-        if (!params.token.sub) {
-          throw new Error("no sub");
-        }
-
-        const sessionToken = uuid();
-
-        const adapter = DrizzleAdapter(db, {
-          usersTable: users,
-          accountsTable: accounts,
-          sessionsTable: sessions,
-          verificationTokensTable: verificationTokens,
-        });
-
-        const session = await adapter.createSession!({
-          userId: params.token.sub,
-          sessionToken: sessionToken,
-          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
-        });
-
-        if (!session) throw new Error("session not created");
-        return sessionToken;
-      }
-      return defaultEncode(params);
-    },
-  },
-  pages: {
-    signIn: "/login", // Redirect here if not authenticated
-    // signOut: "/", // Redirect here after signing out
-    // error: "/auth/error", // Redirect here on error (e.g., failed login)
-    // verifyRequest: "/auth/verify", // Redirect here for email verification
-    // newUser: "/", // Redirect here after initial sign-up
-  },
-  // secret: env.AUTH_SECRET,
-  debug: true,
-  // debug: true
+  secret: env.AUTH_SECRET,
+  // debug: true,
 } satisfies NextAuthConfig;
--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -9,6 +9,16 @@ import {
 import { authConfig } from "./config";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "~/server/db";
+import { v4 as uuid } from "uuid";
+import { encode as defaultEncode } from "next-auth/jwt";
+
+const adapter = DrizzleAdapter(db, {
+  usersTable: users,
+  accountsTable: accounts,
+  sessionsTable: sessions,
+  verificationTokensTable: verificationTokens,
+});
+
 
 const {
   auth: uncachedAuth,
@@ -16,13 +26,54 @@ const {
   signIn,
   signOut,
 } = NextAuth({
-  adapter: DrizzleAdapter(db, {
-    usersTable: users,
-    accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
-  }),
-  //   session: { strategy: "jwt" },
+  adapter: adapter,
+//   session: { strategy: "database" },
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+        // role: user.role
+      },
+    }),
+    jwt: ({ token, account }) => {
+      console.log(`[config][jwt][token]: ${JSON.stringify(token, null, 2)}`)
+      console.log(`[config][jwt][account]: ${JSON.stringify(account, null, 2)}`)
+      if (account?.provider == "credentials") {
+        token.credentials = true;
+      }
+      return token;
+    },
+  },
+  jwt: {
+    encode: async (params) => {
+      console.log(`[config][encode][params]: ${JSON.stringify(params, null, 2)}`)
+      if (params.token?.credentials) {
+        if (!params.token.sub) {
+          throw new Error("no sub");
+        }
+
+        const sessionToken = uuid();
+        const session = await adapter.createSession!({
+          userId: params.token.sub,
+          sessionToken: sessionToken,
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+        });
+
+        if (!session) throw new Error("session not created");
+        return sessionToken;
+      }
+      return defaultEncode(params);
+    },
+  },
+  pages: {
+    signIn: "/login", // Redirect here if not authenticated
+    // signOut: "/", // Redirect here after signing out
+    // error: "/auth/error", // Redirect here on error (e.g., failed login)
+    // verifyRequest: "/auth/verify", // Redirect here for email verification
+    // newUser: "/", // Redirect here after initial sign-up
+  },
   ...authConfig,
 });
 

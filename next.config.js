21c959a246148847a7d1b7482931bdfd04fa50eb
Junior Tan <junior.tan@live.com>
2025-02-15 18:00:40 +0800

    replace with r3 init

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
 
-import redirects from './redirects.js'
+/** @type {import("next").NextConfig} */
+const config = {};
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
-  },
-  reactStrictMode: true,
-  redirects,
-}
-
-export default withPayload(nextConfig)
+export default config;

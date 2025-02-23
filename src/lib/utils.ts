342f470eb432f19b295d9df66ccffe2edd43812a
Junior Tan <junior.tan@live.com>
2025-02-24 01:25:48 +0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function capitalize(s: string): string {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+export type ValueOf<Obj> = Obj[keyof Obj];
+export type OneOnly<Obj, Key extends keyof Obj> = {
+  [key in Exclude<keyof Obj, Key>]: null;
+} & Pick<Obj, Key>;
+export type OneOfByKey<Obj> = { [key in keyof Obj]: OneOnly<Obj, key> };
+export type OneOfType<Obj> = ValueOf<OneOfByKey<Obj>>;

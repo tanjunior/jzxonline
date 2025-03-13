fbc87ef9833fb7ea272b16bc3fe0cab3c95fae5f
Junior Tan <junior.tan@live.com>
2025-03-13 21:17:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: {
+    viewTransition: true,
+    scrollRestoration: true,
+  },
+};
 
 export default config;

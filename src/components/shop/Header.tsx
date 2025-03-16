b83a68eef1b442db9c22e036640df1210b230521
Junior Tan <junior.tan@live.com>
2025-03-16 18:00:07 +0800

    new header

--- src/components/shop/Header.tsx
+++ src/components/shop/Header.tsx
@@ -1,27 +1,219 @@
-import React from "react";
-import { Separator } from "../ui/separator";
-import { NavigationMenuDemo } from "./Nav";
-import Image from "next/image";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+"use client";
 import Link from "next/link";
+import { Logo } from "../Logo";
+import { Menu, X } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import React from "react";
+import { cn } from "@/lib/utils";
 import Cart from "./Cart";
+import Avatar from "./Avatar";
+import { Session } from "next-auth";
 
-export default function ShopHeader() {
+const menuItems = [
+  { name: "Home", href: "/" },
+  { name: "Products", href: "/product" },
+  {
+    name: "Categories",
+    menu: [
+      {
+        title: "Alert Dialog",
+        href: "/docs/primitives/alert-dialog",
+        description:
+          "A modal dialog that interrupts the user with important content and expects a response.",
+      },
+      {
+        title: "Hover Card",
+        href: "/docs/primitives/hover-card",
+        description:
+          "For sighted users to preview content available behind a link.",
+      },
+      {
+        title: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+          "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      },
+      {
+        title: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content.",
+      },
+      {
+        title: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+          "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      },
+      {
+        title: "Tooltip",
+        href: "/docs/primitives/tooltip",
+        description:
+          "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      },
+    ],
+  },
+  { name: "About", href: "/about" },
+];
+
+export const Header = ({ session }: { session: Session | null }) => {
+  const [menuState, setMenuState] = React.useState(false);
+  const [isScrolled, setIsScrolled] = React.useState(false);
+  const style = cn(
+    navigationMenuTriggerStyle(),
+    "text-xl bg-transparent text-muted-foreground hover:bg-transparent hover:text-accent-foreground focus:bg-transparent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=open]:text-accent-foreground data-[state=open]:bg-transparent data-[state=open]:hover:bg-transparent data-[state=open]:focus:bg-transparent",
+  );
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header className="sticky top-0 z-20 mx-4 flex h-16 shrink-0 items-center justify-evenly gap-2 bg-transparent">
-      <Link href={"/"}>
-        <Image alt="LOGO" src="/logo.webp" width={132} height={64} priority />
-      </Link>
-      <div className="w-full place-items-center">
-        <NavigationMenuDemo />
-      </div>
+    <header>
+      <nav
+        data-state={menuState && "active"}
+        className="fixed z-20 w-full px-2"
+      >
+        <div
+          className={cn(
+            "mx-auto mt-2 px-6 transition-all duration-300 lg:px-12",
+            isScrolled &&
+              "max-w-4xl rounded-2xl border bg-background/50 backdrop-blur-lg lg:px-5",
+          )}
+        >
+          <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+            <div className="flex w-full justify-between lg:w-auto">
+              <Link
+                href="/"
+                aria-label="home"
+                className="flex items-center space-x-2"
+              >
+                <Logo />
+              </Link>
 
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <Separator orientation="vertical" className="mx-2 h-4" />
-      <Cart />
+              <button
+                onClick={() => setMenuState(!menuState)}
+                aria-label={menuState == true ? "Close Menu" : "Open Menu"}
+                className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+              >
+                <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
+                <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
+              </button>
+            </div>
+
+            <NavigationMenu className="absolute inset-0 m-auto hidden size-fit lg:block">
+              <NavigationMenuList className="flex gap-8 text-sm">
+                {menuItems.map((item, index) => (
+                  <NavigationMenuItem key={index}>
+                    {item.href ? (
+                      <Link href={item.href} legacyBehavior passHref>
+                        <NavigationMenuLink className={style}>
+                          {item.name}
+                        </NavigationMenuLink>
+                      </Link>
+                    ) : (
+                      <>
+                        <NavigationMenuTrigger className={style}>
+                          {item.name}
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                          <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                            {item.menu!.map((component) => (
+                              <ListItem
+                                key={component.title}
+                                title={component.title}
+                                href={component.href}
+                              >
+                                {component.description}
+                              </ListItem>
+                            ))}
+                          </ul>
+                        </NavigationMenuContent>
+                      </>
+                    )}
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuList>
+            </NavigationMenu>
+
+            <div className="in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border bg-background p-6 shadow-2xl shadow-zinc-300/20 dark:shadow-none md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:lg:bg-transparent">
+              <NavigationMenu className="lg:hidden">
+                <NavigationMenuList className="space-y-6 text-base">
+                  {menuItems.map((item, index) => (
+                    <NavigationMenuItem key={index}>
+                      {item.href ? (
+                        <Link href={item.href} legacyBehavior passHref>
+                          <NavigationMenuLink className={style}>
+                            {item.name}
+                          </NavigationMenuLink>
+                        </Link>
+                      ) : (
+                        <>
+                          <NavigationMenuTrigger className={style}>
+                            {item.name}
+                          </NavigationMenuTrigger>
+                          <NavigationMenuContent>
+                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                              {item.menu!.map((component) => (
+                                <ListItem
+                                  key={component.title}
+                                  title={component.title}
+                                  href={component.href}
+                                >
+                                  {component.description}
+                                </ListItem>
+                              ))}
+                            </ul>
+                          </NavigationMenuContent>
+                        </>
+                      )}
+                    </NavigationMenuItem>
+                  ))}
+                </NavigationMenuList>
+              </NavigationMenu>
+              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+                <Avatar session={session} />
+                <Cart />
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
   );
-}
+};
+
+const ListItem = React.forwardRef<
+  React.ComponentRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";

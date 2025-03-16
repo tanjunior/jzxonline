de17d9611e97945e494ee1476bc6b78a3e1bcf77
Junior Tan <junior.tan@live.com>
2025-03-16 18:01:05 +0800

    Update page.tsx

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -1,5 +1,9 @@
+import { redirect } from "next/navigation";
 import React from "react";
+import { auth } from "~/server/auth";
 
-export default function page() {
+export default async function page() {
+  const session = await auth();
+  if (!session) redirect("/login");
   return <div>page</div>;
 }

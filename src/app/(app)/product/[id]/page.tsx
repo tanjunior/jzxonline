d1a53c0d9afe81619f49868f22ea27c63b7637be
Junior Tan <junior.tan@live.com>
2025-03-30 19:09:11 +0800

    no client

--- src/app/(app)/product/[id]/page.tsx
+++ src/app/(app)/product/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { api } from "~/trpc/server";
 import BackButton from "./BackButton";
-import Client from "./client";
 import { Suspense } from "react";
+import Image from "next/image";
+import { unstable_ViewTransition as ViewTransition } from "react";
+import AddToCart from "./AddToCart";
+import { Badge } from "~/components/ui/badge";
 
 export const dynamic = "force-static";
 
@@ -12,11 +15,42 @@ export default async function Page({
 }) {
   const { id } = await params;
 
-  void api.product.getProductById.prefetch({ id: parseInt(id) });
+  const product = await api.product.getProductById({ id: parseInt(id) });
+
+  if (!product) {
+    return null;
+  }
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <Client id={parseInt(id)} />
+      <div className="grid gap-8 md:grid-cols-2">
+        <div className="relative aspect-square">
+          <ViewTransition name={`item-${id}`}>
+            <Image
+              src={"/placeholder.png"}
+              alt={product.name}
+              fill
+              priority
+              className="rounded-lg object-cover"
+              id={`item-${id}`}
+            />
+          </ViewTransition>
+        </div>
+        <div className="flex flex-col justify-between">
+          <div>
+            <h1 className="mb-2 text-3xl font-bold">{product.name}</h1>
+            <p className="mb-4 text-xl font-semibold text-primary">
+              ${product.price}
+            </p>
+            <p className="mb-4 text-gray-600">{product.description}</p>
+            <div className="mb-4 flex gap-2">
+              <Badge variant="secondary">{product.categoryId}</Badge>
+              {/* <Badge variant="outline">{product.subCategory}</Badge> */}
+            </div>
+          </div>
+          <AddToCart {...product} />
+        </div>
+      </div>
       <Suspense>
         <BackButton />
       </Suspense>

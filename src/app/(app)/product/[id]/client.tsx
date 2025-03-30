c3c120276d799e2131f3c84a508d9026aae9713c
Junior Tan <junior.tan@live.com>
2025-03-30 18:34:10 +0800

    Update client.tsx

--- src/app/(app)/product/[id]/client.tsx
+++ src/app/(app)/product/[id]/client.tsx
@@ -8,11 +8,11 @@ import AddToCart from "./AddToCart";
 import { Badge } from "~/components/ui/badge";
 
 export default function client({ id }: { id: number }) {
-  const [product] = api.product.getProductById.useSuspenseQuery({ id });
+  // const [product] = api.product.getProductById.useSuspenseQuery({ id });
 
-  if (!product) {
-    return null;
-  }
+  // if (!product) {
+  //   return null;
+  // }
 
   return (
     <div className="grid gap-8 md:grid-cols-2">
@@ -30,17 +30,17 @@ export default function client({ id }: { id: number }) {
       </div>
       <div className="flex flex-col justify-between">
         <div>
-          <h1 className="mb-2 text-3xl font-bold">{product.name}</h1>
+          {/* <h1 className="mb-2 text-3xl font-bold">{product.name}</h1>
           <p className="mb-4 text-xl font-semibold text-primary">
             ${product.price}
-          </p>
-          <p className="mb-4 text-gray-600">{product.description}</p>
+          </p> */}
+          {/* <p className="mb-4 text-gray-600">{product.description}</p> */}
           <div className="mb-4 flex gap-2">
-            <Badge variant="secondary">{product.categoryId}</Badge>
+            {/* <Badge variant="secondary">{product.categoryId}</Badge> */}
             {/* <Badge variant="outline">{product.subCategory}</Badge> */}
           </div>
         </div>
-        <AddToCart {...product} />
+        {/* <AddToCart {...product} /> */}
       </div>
     </div>
   );
